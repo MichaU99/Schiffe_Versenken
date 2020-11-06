e86c2251f66a876ac5cd5d964717bd9401b96981
matthias-p <53579673+matthias-p@users.noreply.github.com>
2020-11-06 10:26:51 +0100

    Add files via upload

--- src/game/Field.java
+++ src/game/Field.java
@@ -1,12 +1,22 @@
 package game;
 
+import game.cells.Block;
+import game.cells.Cell;
+import game.cells.Ship;
+import game.cells.Shot;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Field implements FieldInterface, Serializable {
     private Cell[][] playfield;
     private int height;
     private int length;
+
+    public Cell[][] getPlayfield() {
+        return playfield;
+    }
 
     public int getHeight() {
         return height;
@@ -16,9 +26,9 @@ public class Field implements FieldInterface, Serializable {
         return length;
     }
 
-    public Field(int height, int length){
-        assert 0 <= height && height <= 30 : "height not in range 5-30";
-        assert 0 <= length && length <= 30 : "length not in range 5-30";
+    public Field(int height, int length) {
+        assert 5 <= height && height <= 30 : "height not in range 5-30";
+        assert 5 <= length && length <= 30 : "length not in range 5-30";
 
         this.playfield = new Cell[height][length];
         this.height = height;
@@ -26,7 +36,7 @@ public class Field implements FieldInterface, Serializable {
         this.resetField();
     }
 
-    public void resetField(){
+    private void resetField() {
         for (int i = 0; i < this.height; i++) {
             for (int j = 0; j < this.length; j++) {
                 this.playfield[i][j] = new Cell();
@@ -34,19 +44,50 @@ public class Field implements FieldInterface, Serializable {
         }
     }
 
-    public boolean addShip(Ship ship){
-        // check if any position of ship is already occupied
-        for(Position position: ship.getPositions()){
-            if (this.playfield[position.getY()][position.getX()].getClass() != Cell.class)
-                return false;
-        }
+    private ArrayList<Ship> extractShips(int newHeight, int newLength) {
+        int height;
+        int length;
+        ArrayList<Ship> ships = new ArrayList<>();
+        height = Math.min(this.height, newHeight);
+        length = Math.min(this.length, newLength);
 
-        // place ship on playfield
-        for(Position position: ship.getPositions()){
-            this.playfield[position.getY()][position.getX()] = ship;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < length; j++) {
+                if (this.playfield[i][j] instanceof Ship) {
+                    Ship ship = (Ship) this.playfield[i][j];
+                    boolean inBounds = true;
+                    for (Position position: ship.getPositions()) {
+                        if (position.getX() >= newLength || position.getY() >= newHeight) {
+                            inBounds = false;
+                            break;
+                        }
+                    }
+                    if (inBounds && !ships.contains(ship))
+                        ships.add(ship);
+                }
+            }
         }
+        return ships;
+    }
 
-        // block fields arround ship
+    public void resizeField(int newHeight, int newLength) {
+        if (newHeight == this.height && newLength == this.length)
+            return;
+
+        Cell[][] tempField = new Cell[newHeight][newLength];
+        ArrayList<Ship> ships = this.extractShips(newHeight, newLength);
+
+        this.playfield = tempField;
+        this.height = newHeight;
+        this.length = newLength;
+        this.resetField();
+
+        for (Ship ship: ships) {
+            this.addShip(ship);
+        }
+    }
+
+    private void blockFields(Ship ship) {
         for(Position position: ship.getPositions()) {
             if (position.getX() > 0 && this.playfield[position.getY()][position.getX() - 1].getClass() == Cell.class)
                 this.playfield[position.getY()][position.getX() - 1] = new Block();
@@ -71,7 +112,25 @@ public class Field implements FieldInterface, Serializable {
             if (position.getX() < this.length - 1 && position.getY() > 0 &&
                     this.playfield[position.getY() - 1][position.getX() + 1].getClass() == Cell.class)
                 this.playfield[position.getY() - 1][position.getX() + 1] = new Block();
-            }
+        }
+    }
+
+    public boolean addShip(Ship ship){
+        // check if any position of ship is already occupied
+        for(Position position: ship.getPositions()){
+            if (this.playfield[position.getY()][position.getX()].getClass() == Shot.class)
+                continue;
+            if (this.playfield[position.getY()][position.getX()].getClass() != Cell.class)
+                return false;
+        }
+
+        // place ship on playfield
+        for(Position position: ship.getPositions()){
+            this.playfield[position.getY()][position.getX()] = ship;
+        }
+
+        // block fields arround ship
+        this.blockFields(ship);
         return true;
     }
 
@@ -82,7 +141,7 @@ public class Field implements FieldInterface, Serializable {
         int loopcount = 0;
 
         while (!placed){
-            if (loopcount > 5000) {
+            if (loopcount > 7000) {
                 return false;
             }
 
@@ -102,7 +161,7 @@ public class Field implements FieldInterface, Serializable {
                     placed = this.addShip(new Ship(positions));
                 }
                 case 's' -> {
-                    if (startPos.getY() + length > this.height - 1) {
+                    if (startPos.getY() + length > this.height) {
                         continue;
                     }
                     positions[0] = startPos;
@@ -122,7 +181,7 @@ public class Field implements FieldInterface, Serializable {
                     placed = this.addShip(new Ship(positions));
                 }
                 case 'e' -> {
-                    if (startPos.getX() + length > this.length - 1) {
+                    if (startPos.getX() + length > this.length) {
                         continue;
                     }
                     positions[0] = startPos;
@@ -138,19 +197,50 @@ public class Field implements FieldInterface, Serializable {
     }
 
     public boolean addShipRandom(int [] lengths){
-        for (int length: lengths){
-            if (!addShipRandom(length)){
-                // Fallback if ships can't be placed because of bad previous rng or exceeding amount of tries
+        for (int i = 0; i < lengths.length; i++) {
+            if (!addShipRandom(lengths[i])) {
+                System.out.println("Retry");
                 this.resetField();
-                this.addShipRandom(lengths);
-                break;
+                i = -1;
             }
         }
+        assert this.getShipCount() == lengths.length : "Nicht alle Schiffe plaziert";
         return true;
+    }
+
+    public int getShipCount() {
+        // returns how many ships are on the current field
+        return this.extractShips(this.height, this.length).size();
+    }
+
+    public int[] getShipLengths() {
+        // returns the lengths of every ship on the current field as array
+        ArrayList<Ship> extractShips = this.extractShips(this.height, this.length);
+        int[] shipLengths = new int[extractShips.size()];
+        for (int i = 0; i < extractShips.size(); i++) {
+            shipLengths[i] = extractShips.get(i).getLength();
+        }
+
+        for (int i = 0; i < shipLengths.length - 1; i++) {
+            if (shipLengths[i + 1] > shipLengths[i]) {
+                int temp = shipLengths[i + 1];
+                shipLengths[i + 1] = shipLengths[i];
+                shipLengths[i] = temp;
+            }
+        }
+
+        return shipLengths;
     }
 
     public int registerShot(Position position){
         Cell cell = this.playfield[position.getY()][position.getX()];
+        this.playfield[position.getY()][position.getX()] = new Shot();
+
+        if (cell instanceof Ship) {
+            Shot s = (Shot) this.playfield[position.getY()][position.getX()];
+            s.setWasShip(true);
+        }
+
         return cell.shot();
     }
 
@@ -163,6 +253,25 @@ public class Field implements FieldInterface, Serializable {
             System.out.print(i + 1);
             for (Cell cell: this.playfield[i]){
                 System.out.print("\t" + cell.toString());
+            }
+            System.out.println();
+        }
+    }
+
+    public void printFieldConcealed() {
+        for (int i = 65; i < this.length + 65; i++) {
+            System.out.print("\t" + (char)i);
+        }
+        System.out.println();
+        for (int i = 0; i < this.height; i++) {
+            System.out.print(i + 1);
+            for (Cell cell: this.playfield[i]){
+                if (cell instanceof Shot) {
+                    System.out.print("\t" + cell.toString());
+                }
+                else {
+                    System.out.print("\t 0");
+                }
             }
             System.out.println();
         }
--- src/game/Game.java
+++ src/game/Game.java
@@ -1,21 +1,12 @@
 package game;
 
-import enums.KiStrength;
-import enums.MultiplayerMode;
-import ki.Ki;
-import network.BattleshipProtocol;
-import network.Client;
-import network.Server;
+import game.cells.Ship;
 
 import java.io.*;
 
-public class Game implements Serializable {
-    private Field field;
-    private Field enemyField;
-    private Client client;
-    private Server server;
-    private Ki ki;
-    private int[] shipLengths;
+public abstract class Game implements Serializable {
+    protected Field field;
+    protected Field enemyField;
 
     public Field getField() {
         return field;
@@ -25,8 +16,10 @@ public class Game implements Serializable {
         this.field = field;
     }
 
-    public void setShipLengths(int[] shipLengths) {
-        this.shipLengths = shipLengths;
+    public Field getEnemyField() { return enemyField; }
+
+    public Game() {
+
     }
 
     public Game(int playFieldHeight, int playFieldLength){
@@ -34,63 +27,20 @@ public class Game implements Serializable {
         this.enemyField = new Field(playFieldHeight, playFieldLength);
     }
 
-    public Game(int playFieldHeight, int playFieldLength, int[] shipLengths){
-        this(playFieldHeight, playFieldLength);
-        this.shipLengths = shipLengths;
+    public boolean addShip(Position[] positions) {
+        return this.field.addShip(new Ship(positions));
     }
 
-    public Game(int playFieldHeight, int playFieldLength, int[] shipLengths, KiStrength kiStrength){
-        this(playFieldHeight, playFieldLength, shipLengths);
-        this.ki = new Ki(this.field, this.enemyField, this.shipLengths, kiStrength);
-    }
+    // public abstract void shoot();
 
-    public Game(Object... args){
-        assert args[0].getClass().isEnum() : "First Parameter has to be enum";
-        switch (((MultiplayerMode) args[0])) {
-            case CLIENT -> {
-                assert args[1].getClass() == String.class: "Second parameter is hostname (String)";
-                assert args[2].getClass() == Integer.class: "Thrid parameter is portnumber (Int)";
+    public abstract void loadGame(String id) throws IOException, ClassNotFoundException;
 
-                this.client = new Client();
-                this.client.setHostname((String) args[1]);
-                this.client.setPortnumber((Integer) args[2]);
-            }
-            case SERVER -> {
-                this.server = new Server();
-                this.server.setPortNumber((Integer) args[4]);
-                this.field = new Field((Integer) args[1], (Integer) args[2]);
-                this.enemyField = new Field((Integer) args[1], (Integer) args[2]);
-                this.shipLengths = (int[]) args[3];
-
-                this.server.waitForConnection();
-                this.server.writeLine(BattleshipProtocol.formatSize(this.field.getLength(), this.field.getHeight()));
-                this.server.writeLine(BattleshipProtocol.formatShips(this.shipLengths));
-            }
-        }
-    }
-
-    public void loadGame(int id){
-        try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(id + ".txt"));
-            Game temp = (Game)in.readObject();
-            this.field = temp.field;
-            this.enemyField = temp.enemyField;
-            this.ki = temp.ki;
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void saveGame(int id){
-        try {
-            FileOutputStream fout = new FileOutputStream(id + ".txt");
-            ObjectOutputStream out = new ObjectOutputStream(fout);
-            out.writeObject(this);
-            out.flush();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void saveGame(String id) throws IOException {
+        FileOutputStream fout = new FileOutputStream(id + ".txt");
+        ObjectOutputStream out = new ObjectOutputStream(fout);
+        out.writeObject(this);
+        out.flush();
+        out.close();
     }
 
 }
