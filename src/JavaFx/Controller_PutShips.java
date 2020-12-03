1d3f23634c415d60bfcd2fae78dfe4cadf3d7d0a
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-12-03 13:17:06 +0100

    Added PutShips basic Layout, added switch between putships and newgame

--- src/JavaFx/Controller_PutShips.java
+++ src/JavaFx/Controller_PutShips.java
@@ -1,4 +1,28 @@
 package JavaFx;
 
-public class Controller_PutShips {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_PutShips implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    public void backToStart(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("Layout_NewGame.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/JavaFx/Controller_StartingScreens.java
+++ src/JavaFx/Controller_StartingScreens.java
@@ -18,7 +18,7 @@ public class Controller_StartingScreens implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {    }
 
-    public void changeToNewGame(ActionEvent event) throws IOException {
+    public void changeToNewGame(ActionEvent event) throws IOException { //Wechselt die Szene von Start zu NewGame
         Parent  root= FXMLLoader.load(getClass().getResource("Layout_NewGame.fxml"));
         Scene scene = new Scene(root);
 
@@ -27,8 +27,16 @@ public class Controller_StartingScreens implements Initializable {
         stage.show();
     }
 
-    public void backToStart(ActionEvent event) throws IOException{
+    public void backToStart(ActionEvent event) throws IOException{ //Wechselt die Szene von NewGame zu Start
         Parent  root= FXMLLoader.load(getClass().getResource("Layout_Start.fxml"));
+        Scene scene = new Scene(root);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void changeToSingleGamae(ActionEvent event) throws IOException{//Wechselt die Szene von NewGame zu PutShips
+        Parent  root= FXMLLoader.load(getClass().getResource("Layout_PutShips.fxml"));
         Scene scene = new Scene(root);
 
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
