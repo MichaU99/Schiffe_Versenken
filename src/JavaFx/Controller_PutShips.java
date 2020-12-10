fe8ab66f48c5683074729a1bb5ff6a5b672de51d
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2020-12-10 20:37:17 +0100

    Added new GridPane

--- src/JavaFx/Controller_PutShips.java
+++ src/JavaFx/Controller_PutShips.java
@@ -1,11 +1,14 @@
 package JavaFx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +16,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller_PutShips implements Initializable {
+    @FXML
+    private GridPane GridP;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        for(int x=0;x<10;x++){
+            for(int y=0;y<10;y++){
+                Label l=new Label("Hallo ;)");
+                GridPane.setConstraints(l,x,y);
+                GridP.getChildren().add(l);
+            }
+        }
+        Object[] array=GridP.getChildren().toArray();
+        //GridPane.getColumnIndex
+        //GridPane.getRowIndex
     }
 
     public void backToStart(ActionEvent event) throws IOException {
