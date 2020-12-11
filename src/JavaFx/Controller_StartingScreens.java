dc57b2d568b5ea7437d95d341ba7395ad4fc2f31
Veronika <veronika_scheller@web.de>
2020-12-11 11:49:34 +0100

    start Options gemacht

--- src/JavaFx/Controller_StartingScreens.java
+++ src/JavaFx/Controller_StartingScreens.java
@@ -67,6 +67,14 @@ public class Controller_StartingScreens implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+    public void changeToStartOptions(ActionEvent event) throws IOException{ // Wechselt in die Szene von Startoptions
+        Parent  root= FXMLLoader.load(getClass().getResource("Layout_Start_Options.fxml"));
+        Scene scene = new Scene(root);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
     public void close(ActionEvent event){
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
