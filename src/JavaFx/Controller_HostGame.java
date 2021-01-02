172051c70fb0edf19b1cf4d9b5eabb5147b339a3
Matthias Pijarowski <pijarowski.matthias@gmail.com>
2021-01-02 20:09:04 +0100

    onlinegame implementation

--- src/JavaFx/Controller_HostGame.java
+++ src/JavaFx/Controller_HostGame.java
@@ -1,15 +1,38 @@
 package JavaFx;
 
+import enums.KiStrength;
+import game.OnlineHostGame;
+import gui.GameOptions;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller_HostGame {
     public Stage stage;
+
+    @FXML
+    private TextField fsTbx;
+    @FXML
+    private TextField portTbx;
+    @FXML
+    private TextField fiveTbx;
+    @FXML
+    private TextField fourTbx;
+    @FXML
+    private TextField threeTbx;
+    @FXML
+    private TextField twoTbx;
+
     public void changeToMultGameChooseRole(ActionEvent event) throws IOException {//Wechselt die Szene von NewGame zu PutShips
         Parent root= FXMLLoader.load(getClass().getResource("Layout_Mult_ChooseRole.fxml"));
         Scene scene = new Scene(root);
@@ -18,12 +41,135 @@ public class Controller_HostGame {
         stage.setScene(scene);
         stage.show();
     }
+
     public void changeToLoadingScreen(ActionEvent event) throws IOException {//Wechselt die Szene von NewGame zu PutShips
+
+        OnlineHostGame game = validateInput();
+        if (game == null) {
+            return;
+        } else {
+            Controller_LoadingScreen.onlineGame = game;
+        }
+
         Parent root= FXMLLoader.load(getClass().getResource("Layout_LoadingScreen.fxml"));
         Scene scene = new Scene(root);
 
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    private OnlineHostGame validateInput() {
+        int status = 0;
+        try {
+            int fs = Integer.parseInt(this.fsTbx.getText());
+            if (fs > 30 || fs < 5) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            status = 1;
+            int pn = Integer.parseInt(portTbx.getText());
+            if (pn < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            status = 2;
+            int five = Integer.parseInt(fiveTbx.getText());
+            if (five < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            status = 3;
+            int four = Integer.parseInt(fourTbx.getText());
+            if (four < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            status = 4;
+            int three = Integer.parseInt(threeTbx.getText());
+            if (three < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            status = 5;
+            int two = Integer.parseInt(twoTbx.getText());
+            if (two < 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            ArrayList<Integer> shipLengthsAr = new ArrayList<>();
+            for (int i = 0; i < five; i++) {
+                shipLengthsAr.add(5);
+            }
+            for (int i = 0; i < four; i++) {
+                shipLengthsAr.add(4);
+            }
+            for (int i = 0; i < three; i++) {
+                shipLengthsAr.add(3);
+            }
+            for (int i = 0; i < two; i++) {
+                shipLengthsAr.add(2);
+            }
+
+            int[] shipLengths = new int[shipLengthsAr.size()];
+            for (int i = 0; i < shipLengthsAr.size(); i++) {
+                shipLengths[i] = shipLengthsAr.get(i);
+            }
+
+            gui.GameOptions go = new GameOptions(fs, KiStrength.INTERMEDIATE, five, four, three, two);
+            OnlineHostGame hostGame = new OnlineHostGame(fs, fs, pn, shipLengths, go);
+            return hostGame;
+        } catch (NumberFormatException e) {
+            switch (status) {
+                case 0:
+                    onTbxError(fsTbx, 3000);
+                    break;
+                case 1:
+                    onTbxError(portTbx, 3000);
+                    break;
+                case 2:
+                    onTbxError(fiveTbx, 3000);
+                    break;
+                case 3:
+                    onTbxError(fourTbx, 3000);
+                    break;
+                case 4:
+                    onTbxError(threeTbx, 3000);
+                    break;
+                case 5:
+                    onTbxError(twoTbx, 3000);
+                    break;
+                default:
+                    break;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            switch (status) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+        }
+        return null;
+    }
+
+    private void onTbxError(javafx.scene.control.TextField textField, int duration) {
+        textField.setStyle("-fx-text-inner-color: red; -fx-border-color: red; -fx-border-radius: 2 2 2 2; -fx-background-radius: 2 2 2 2;");
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                textField.setStyle("-fx-text-inner-color: black; -fx-border-color: transparent;");
+            }
+        }, duration);
     }
 }
--- src/JavaFx/Controller_LoadingScreen.java
+++ src/JavaFx/Controller_LoadingScreen.java
@@ -1,4 +1,110 @@
 package JavaFx;
 
+import game.OnlineClientGame;
+import game.OnlineGame;
+import game.OnlineHostGame;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Controller_LoadingScreen {
+    public Stage stage;
+    public static OnlineGame onlineGame;
+    private Timer timer;
+
+    @FXML
+    private Label statusLbl;
+
+    public void initialize() {
+        if (onlineGame instanceof OnlineHostGame) {
+            waitForConnection((OnlineHostGame) onlineGame);
+        } else if (onlineGame instanceof OnlineClientGame) {
+            connect((OnlineClientGame) onlineGame);
+        }
+    }
+
+    public void changeToPutShips(ActionEvent event) throws IOException {//Wechselt die Szene von NewGame zu PutShips
+        Parent root= FXMLLoader.load(getClass().getResource("Layout_PutShips.fxml"));
+        Scene scene = new Scene(root);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void connect(OnlineClientGame clientGame) {
+        final int[] counter = {0};
+        updateStatusLabel("Trying to connect!");
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                while (!clientGame.establishConnection()) {
+                    counter[0]++;
+                    Platform.runLater(() -> updateStatusLabel("Fail, retry: " + counter[0]));
+                }
+                updateStatusLabel("Success!");
+
+                try {
+                    Parent root= FXMLLoader.load(getClass().getResource("Layout_PutShips.fxml"));
+                    Scene scene = new Scene(root);
+                    Stage stage = (Stage) statusLbl.getScene().getWindow();
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 500);
+    }
+
+    private void waitForConnection(OnlineHostGame hostGame) {
+        updateStatusLabel("Waiting for Connection!");
+        new Thread(() -> {
+            boolean status = hostGame.waitForConnection();
+
+            if (status) {
+                try {
+                    Parent root= FXMLLoader.load(getClass().getResource("Layout_PutShips.fxml"));
+                    Scene scene = new Scene(root);
+                    Stage stage = (Stage) statusLbl.getScene().getWindow();
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void onBackBtnClick(ActionEvent event) throws IOException {
+        Parent root = null;
+        if (onlineGame instanceof OnlineHostGame) {
+            //onlineGame.freeSocket();
+            //timer.cancel();
+            root = FXMLLoader.load(getClass().getResource("Layout_HostGame.fxml"));
+        } else if (onlineGame instanceof OnlineClientGame) {
+            root = FXMLLoader.load(getClass().getResource("NewGame_Muliti_Client.fxml"));
+        }
+
+        Scene scene = new Scene(root);
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void updateStatusLabel(String text) {
+        this.statusLbl.setText(text);
+    }
 }
