e38ed93f608d1fc73818e931333e484ebb694df2
MichaU99 <50555533+MichaU99@users.noreply.github.com>
2021-01-02 16:41:56 +0100

    communication

--- src/JavaFx/Controller_GameScreen.java
+++ src/JavaFx/Controller_GameScreen.java
@@ -3,10 +3,12 @@ package JavaFx;
 import game.*;
 import game.cells.Ship;
 import game.cells.Shot;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -17,15 +19,14 @@ import java.util.ResourceBundle;
 
 public class Controller_GameScreen implements Initializable {
 
-    private boolean[][] knownField;
     private Game game;
-    String waterCell="-fx-background-color: #00BFFF; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
-    String shipCell="-fx-background-color: #000000; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
-    String markCell="-fx-border-width:1em;  -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
+    String waterCell = "-fx-background-color: #00BFFF; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
+    String shipCell = "-fx-background-color: #000000; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
+    String markCell = "-fx-border-width:1em;  -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
     // TODO: 02.01.2021 shotWater und shotShip sollten jeweils ein rotes und dunkelblaues kreuz ohne Hintergrund enthalten 
-    String shotWater="";
-    String shotShip="";
-    Position markedPos=null; //Speichert welche Felder bereits aufgedeckt wurden und welche nicht
+    String shotWater = "-fx-background-color: #060b60; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
+    String shotShip = "-fx-background-color: #e00c0c; -fx-margin: 5 5 5 5;-fx-border-color: #000000;-fx-pref-height: 5em;-fx-pref-width: 5em";
+    Position markedPos = null; //Speichert welche Felder bereits aufgedeckt wurden und welche nicht
 
     @FXML
     private GridPane GP_Player;
@@ -35,63 +36,73 @@ public class Controller_GameScreen implements Initializable {
     private Button Shoot_bt;
 
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        game=Controller_PutShips.getGame();
-        knownField=new boolean[game.getField().getHeight()][game.getField().getLength()];
+        game = Controller_PutShips.getGame();
         updateField(GP_Player);
         game.startGame();
 
         //Unterscheidet zwischen Beobachteten Spielen, in denen beide Felder von anfang an komplett für den Beobachter bekannt sind
         // und normal gespielten Spielern in denen nur ein Feld bekannt ist
-        if(game instanceof KiVsKiGame){
+        if (game instanceof KiVsKiGame) {
             updateFieldDisclosed(GP_Enemy);
-        }
-        else{
+        } else {
             makeFieldEnemy();
             Shoot_bt.setDisable(true);
-            if(game instanceof LocalGame){
+            if (game instanceof LocalGame) {
 
-            }
-            else if(game instanceof OnlineGame){
+            } else if (game instanceof OnlineGame) {
 
             }
         }
-
 
 
         // Object[] array=GP_Player.getChildren().toArray();
         // Object[] array=GP_Enemy.getChildren().toArray();
     }
 
-    public void markField(MouseEvent event){
+    public void markField(MouseEvent event) {
         // TODO: 02.01.2021 Felder sollten gezielt gelöscht und gesetzt werden sonst gibt es Komplikationen mit dickem Rand
-            int x=GridPane.getColumnIndex((Node)event.getTarget());
-            int y=GridPane.getRowIndex((Node)event.getTarget());
-            HBox cell;
-            if(markedPos!=null) {
-                cell = new HBox();
-                if(!knownField[markedPos.getX()][markedPos.getY()]){
-                    cell.setStyle(waterCell);
-                }
-                else if (game.getEnemyField().getCell(markedPos) instanceof Ship) {
+        int x = GridPane.getColumnIndex((Node) event.getTarget());
+        int y = GridPane.getRowIndex((Node) event.getTarget());
+        HBox cell;
+        if (markedPos != null) {
+            cell = new HBox();
+            if (game.getEnemyField().getCell(markedPos) instanceof Shot) { //Unsicher ob Vergleich richtig
+                // TODO: 02.01.2021 Prüfen ob es so funktioniert, soll eigentlich praktisch das Kreuz über die Originalcell legen, etwas fragwürdige umsetzung, vielleicht besser wenn man zwei css addieren kann
+                Shot s = ((Shot) game.getEnemyField().getCell(markedPos));
+                if (s.getWasShip()) {
+                    cell.setStyle(shotShip);
+                    /*
                     cell.setStyle(shipCell);
-                }
-                else if (game.getEnemyField().getCell(markedPos) instanceof Shot) { //Unsicher ob Vergleich richtig
-                    cell.setStyle(shotCell);
+                    GridPane.setConstraints(cell, x, y);
+                    gridPane.getChildren().add(cell);
+                    cell= new HBox();
+                    cell.setStyle(shotShip);
+                    */
                 }
                 else {
+                    cell.setStyle(shotWater);
+                    /*
                     cell.setStyle(waterCell);
+                    GridPane.setConstraints(cell, x, y);
+                    gridPane.getChildren().add(cell);
+                    cell= new HBox();
+                    cell.setStyle(shotWater);
+                    */
                 }
-                GridPane.setConstraints(cell, markedPos.getX(), markedPos.getY());
-                GP_Enemy.getChildren().add(cell);
             }
-            cell=new HBox();
-            markedPos = new Position(x, y);
-            cell.setStyle(markCell);
-            GridPane.setConstraints(cell, markedPos.getX(), markedPos.getY());
+            else {
+                cell.setStyle(waterCell);
+            }
+            GridPane.setConstraints(cell,markedPos.getX(),markedPos.getY());
+            GP_Enemy.getChildren().add(cell);
+        }
+        cell=new HBox();
+        markedPos = new Position(x, y);
+        cell.setStyle(markCell);
+        GridPane.setConstraints(cell, markedPos.getX(), markedPos.getY());
         GP_Enemy.getChildren().add(cell);
         Shoot_bt.setDisable(false);
     }
@@ -112,16 +123,55 @@ public class Controller_GameScreen implements Initializable {
         }
     }
     public void shootbtn(ActionEvent event){
-        if(!game.isMyTurn()) return;
         if(game instanceof LocalGame && game.isMyTurn()) {
             if (markedPos == null) return;
-            knownField[markedPos.getX()][markedPos.getY()] = true; //Kann man mit Backend rausnehmen
             Shoot_bt.setDisable(true);
-            game.shoot(markedPos);
+            int rc=game.shoot(markedPos);
             updateField(GP_Enemy);
+            if(rc==0){//Kein Treffer
+                // TODO: 02.01.2021 Labelchanges sobald endgültiges Feld da ist
+                //LabelChange to Enemy
+                //Label
+            }
+            new Thread(() -> {
+                int rc1;
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (game.whoWon() != -1)
+                        break;
+                    rc1 = game.shoot(null);
+                    updateField(GP_Player);
+                    if (rc1 == 0) {
+                        //Platform.runLater(() -> curPlayerNameLbl.setText(PLAYER1_NAME));
+                        break;
+                    } else if (rc1 == 2) {
+                        Platform.runLater(this::checkGameEnded);
+                    }
+                }
+            }).start();
         }
         else{
-
+            game.shoot(null);
+        }
+    }
+    private void checkGameEnded() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText("Game Ended!");
+        switch (game.whoWon()) {
+            case 0:
+                alert.setContentText("You won!");
+                alert.showAndWait();
+                break;
+            case 1:
+                alert.setContentText("You lost!");
+                alert.showAndWait();
+                break;
+            default:
+                break;
         }
     }
 
@@ -131,7 +181,6 @@ public class Controller_GameScreen implements Initializable {
      * @param gridPane gibt an welches Feld geupdated werden soll
      */
     private void updateField(GridPane gridPane) {
-
         if (GP_Enemy.equals(gridPane)) {
             gridPane.getChildren().clear();
             for (int x = 0; x < game.getField().getLength(); x++) {
@@ -139,20 +188,26 @@ public class Controller_GameScreen implements Initializable {
                     HBox cell = new HBox();
                     // TODO: 02.01.2021 Prüfen ob es so funktioniert, soll eigentlich praktisch das Kreuz über die Originalcell legen, etwas fragwürdige umsetzung, vielleicht besser wenn man zwei css addieren kann
                     if (game.getEnemyField().getCell(new Position(x, y)) instanceof Shot){
-                        Shot s = ((Shot) game.getEnemyField().getPlayfield()[x][y]);
+                        Shot s = ((Shot) game.getEnemyField().getCell(new Position(x,y)));
                         if(s.getWasShip()){
-                            cell.setStyle(shipCell);
-                            GridPane.setConstraints(cell, x, y);
-                            gridPane.getChildren().add(cell);
-                            cell= new HBox();
                             cell.setStyle(shotShip);
+                            /*
+                            cell.setStyle(shipCell);
+                            //GridPane.setConstraints(cell, x, y);
+                            //gridPane.getChildren().add(cell);
+                            //cell= new HBox();
+                            //cell.setStyle(shotShip);
+                            */
                         }
                         else{
+                            cell.setStyle(shotWater);
+                            /*
                             cell.setStyle(waterCell);
                             GridPane.setConstraints(cell, x, y);
                             gridPane.getChildren().add(cell);
                             cell= new HBox();
                             cell.setStyle(shotWater);
+                           */
                         }
                     }
                     else {
@@ -169,19 +224,25 @@ public class Controller_GameScreen implements Initializable {
                 for (int y = 0; y < game.getField().getHeight(); y++) {
                     HBox cell = new HBox();
                     if (game.getField().getCell(new Position(x, y)) instanceof Shot) {
-                        Shot s = ((Shot) game.getEnemyField().getPlayfield()[x][y]);
+                        Shot s = ((Shot) game.getEnemyField().getCell(new Position(x, y)));
                         if (s.getWasShip()) {
-                            cell.setStyle(shipCell);
-                            GridPane.setConstraints(cell, x, y);
-                            gridPane.getChildren().add(cell);
-                            cell = new HBox();
                             cell.setStyle(shotShip);
+                            /*
+                            cell.setStyle(shipCell);
+                            //GridPane.setConstraints(cell, x, y);
+                            //gridPane.getChildren().add(cell);
+                            //cell= new HBox();
+                            //cell.setStyle(shotShip);
+                            */
                         } else {
+                            cell.setStyle(shotWater);
+                            /*
                             cell.setStyle(waterCell);
                             GridPane.setConstraints(cell, x, y);
                             gridPane.getChildren().add(cell);
-                            cell = new HBox();
+                            cell= new HBox();
                             cell.setStyle(shotWater);
+                           */
                         }
                     }
                     else if (game.getField().getCell(new Position(x, y)) instanceof Ship) {
