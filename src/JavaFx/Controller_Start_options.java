4aea1c8b473b61db3de4b7d377691a2fa463107b
Veronika <veronika_scheller@web.de>
2021-01-23 16:18:12 +0100

    controller für start options frisch reingemacht

---
+++ src/JavaFx/Controller_Start_options.java
@@ -0,0 +1,14 @@
+package JavaFx;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_Start_options implements Initializable {
+
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
+
+}
